8a53c9ced772064c0b3d59ae0eeb94ec86d9695f
Clancy-dev <132342072+Clancy-dev@users.noreply.github.com>
2024-06-14 21:24:07 +0300

    add
    am adding

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    setTimeout(function(){
+        document.querySelector("body").style.display="block";
+        document.querySelector(".preloader").style.display="none";
+
+    },4000);
+
+});
